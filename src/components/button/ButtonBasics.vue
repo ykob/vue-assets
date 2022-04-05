60f604c182ae8ba07f610f64c1fbde11a8217e90
Yoichi Kobayashi <info@tplh.net>
2022-04-05 14:16:38 +0900

    added ButtonBasics component.

---
+++ src/components/button/ButtonBasics.vue
@@ -0,0 +1,55 @@
+<template>
+  <component
+    v-bind="$attrs"
+    class="button"
+    :class="classnames"
+    :is="tag"
+    :to="to"
+    @click="$emit('click', $event)"
+  >
+    <slot></slot>
+  </component>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ButtonBasics',
+  props: {
+    buttonType: {
+      type: String,
+      default: 'common',
+    },
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+    tag: {
+      type: String,
+      default: 'button',
+    },
+    to: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    classnames() {
+      return [`${this.buttonType}`]
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.button {
+  appearance: none;
+  box-sizing: border-box;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 0;
+}
+</style>
