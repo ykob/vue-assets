6bdd25c8761b67ac9aa7edeb15de533875458d6a
Yoichi Kobayashi <info@tplh.net>
2022-04-05 21:06:24 +0900

    removed the default view files.

---
+++ src/views/ViewHome.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2>Buttons</h2>
+  <ButtonBasics> Button Basics </ButtonBasics>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import ButtonBasics from '@/components/button/ButtonBasics.vue'
+
+export default defineComponent({
+  name: 'ViewHome',
+  components: {
+    ButtonBasics,
+  },
+})
+</script>
