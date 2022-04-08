e2ad48ceb17a87d274834c0977775a1716b71eff
Yoichi Kobayashi <info@tplh.net>
2022-04-08 23:58:21 +0900

    added container.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <h1>Vue Assets</h1>
-  <router-view />
+  <div class="container mx-auto px-4">
+    <h1>Vue Assets</h1>
+    <router-view />
+  </div>
 </template>
 
 <style lang="scss"></style>
