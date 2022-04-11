9472e068f9e3fee5289436ef42ebd79adfbcb959
Yoichi Kobayashi <info@tplh.net>
2022-04-11 22:55:38 +0900

    added input components.

---
+++ src/components/input/InputText.vue
@@ -0,0 +1,49 @@
+<template>
+  <input
+    v-bind="$attrs"
+    :class="classnames"
+    :style="styles"
+    @input="$emit('input', $event.target.value)"
+    @change="$emit('change', $event.target.value)"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    width: {
+      type: String,
+      default: '100%',
+    },
+    error: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    classnames(): { [key: string]: boolean } {
+      return {
+        error: this.error
+      }
+    },
+    styles(): { [key: string]: string } {
+      return {
+        width: this.width,
+      }
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+input {
+  height: 44px;
+  appearance: none;
+  box-sizing: border-box;
+  padding-right: 16px;
+  padding-left: 16px;
+  border: 1px solid;
+}
+</style>
---
+++ src/components/input/inputMultilineText.vue
@@ -0,0 +1,59 @@
+<template>
+  <textarea
+    v-bind="$attrs"
+    :class="classnames"
+    :style="styles"
+    :value="value"
+    @input="$emit('input', $event.target.value)"
+    @change="$emit('change', $event.target.value)"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+    width: {
+      type: String,
+      default: '100%',
+    },
+    height: {
+      type: String,
+      default: '240px',
+    },
+    error: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    classnames(): { [key: string]: boolean } {
+      return {
+        error: this.error
+      }
+    },
+    styles(): { [key: string]: string } {
+      return {
+        width: this.width,
+        height: this.height,
+      }
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+textarea {
+  appearance: none;
+  box-sizing: border-box;
+  display: block;
+  padding: 16px;
+  border: 1px solid;
+  line-height: 1.5;
+}
+</style>
