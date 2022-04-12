6ea5cd94ec550099c94ac03c8adafb9d1bf3061e
Yoichi Kobayashi <info@tplh.net>
2022-04-12 23:37:26 +0900

    added StylelintPlugin to vue.config.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
+const StylelintPlugin = require('stylelint-webpack-plugin')
 module.exports = defineConfig({
+  configureWebpack: {
+    plugins: [
+      new StylelintPlugin({
+        files: ['**/*.{vue,html,css,scss}'],
+      }),
+    ],
+  },
   transpileDependencies: true,
 })
