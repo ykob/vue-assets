8e53070a7db3e090a60396afc312632b09b2fbef
ykob <info@tplh.net>
2022-04-17 12:43:36 +0900

    added custom color to current and transparent.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     colors: {
+      current: 'currentColor',
+      transparent: 'transparent',
       primary: colors.cyan,
       danger: colors.red,
     },
