449e813c100a0259ba81a8cdc64b13f50ef36d3e
ykob <info@tplh.net>
2022-05-18 09:44:47 +0900

    added the input elements on the home page.

--- src/views/ViewHome.vue
+++ src/views/ViewHome.vue
@@ -1,6 +1,6 @@
 <template>
   <h2 class="mb-4 text-3xl">Buttons</h2>
-  <div class="flex gap-4">
+  <div class="flex gap-4 mb-4">
     <ButtonBasics> Button </ButtonBasics>
     <ButtonBasics class="text-neutral-50 bg-primary-500"> Button </ButtonBasics>
     <ButtonBasics class="text-neutral-50 bg-secondary-500">
@@ -19,16 +19,28 @@
     <ButtonBasics class="text-neutral-50 bg-error-500"> Button </ButtonBasics>
     <ButtonBasics class="text-neutral-50 bg-warning-500"> Button </ButtonBasics>
   </div>
+  <div class="flex flex-col gap-4">
+    <InputText v-model="inputText" />
+    <InputMultilineText v-model="InputMultilineText" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import ButtonBasics from '@/components/button/ButtonBasics.vue'
+import InputMultilineText from '@/components/input/InputMultilineText.vue'
+import InputText from '@/components/input/InputText.vue'
 
 export default defineComponent({
   name: 'ViewHome',
   components: {
     ButtonBasics,
+    InputMultilineText,
+    InputText,
   },
+  data: () => ({
+    inputText: '',
+    InputMultilineText: '',
+  }),
 })
 </script>
