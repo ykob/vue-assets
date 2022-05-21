62607289d5d86d70e0c68bdf078ce76b0867348d
ykob <info@tplh.net>
2022-05-21 11:50:34 +0900

    added ButtonBlock component.

---
+++ src/components/button/ButtonBlock.vue
@@ -0,0 +1,42 @@
+<template>
+  <button-basics
+    v-bind="$attrs"
+    :class="classnames"
+    :tag="tag"
+    :to="to"
+    @click="$emit('click', $event)"
+  >
+    <slot />
+  </button-basics>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import ButtonBasics from './ButtonBasics.vue'
+
+export default defineComponent({
+  name: 'ButtonBlock',
+  components: {
+    ButtonBasics,
+  },
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+    tag: {
+      type: String,
+      default: 'button',
+    },
+    to: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    classnames(): string[] {
+      return ['h-10', 'px-4', 'rounded', 'text-neutral-50', 'w-full']
+    },
+  },
+})
+</script>
--- src/views/ViewHome.vue
+++ src/views/ViewHome.vue
@@ -1,29 +1,29 @@
 <template>
   <h2 class="mb-4 text-3xl">Buttons</h2>
   <div class="flex gap-4 mb-4">
-    <ButtonBasics> Button </ButtonBasics>
-    <ButtonBasics class="text-neutral-50 bg-primary-500"> Button </ButtonBasics>
-    <ButtonBasics class="text-neutral-50 bg-secondary-500">
+    <button-block> Button </button-block>
+    <button-block class="text-neutral-50 bg-primary-500"> Button </button-block>
+    <button-block class="text-neutral-50 bg-secondary-500">
       Button
-    </ButtonBasics>
-    <ButtonBasics class="text-neutral-50 bg-information-500">
+    </button-block>
+    <button-block class="text-neutral-50 bg-information-500">
       Button
-    </ButtonBasics>
-    <ButtonBasics class="text-neutral-50 bg-attention-500">
+    </button-block>
+    <button-block class="text-neutral-50 bg-attention-500">
       Button
-    </ButtonBasics>
-    <ButtonBasics class="text-neutral-50 bg-success-500"> Button </ButtonBasics>
-    <ButtonBasics class="text-neutral-50 bg-destructive-500">
+    </button-block>
+    <button-block class="text-neutral-50 bg-success-500"> Button </button-block>
+    <button-block class="text-neutral-50 bg-destructive-500">
       Button
-    </ButtonBasics>
-    <ButtonBasics class="text-neutral-50 bg-error-500"> Button </ButtonBasics>
-    <ButtonBasics class="text-neutral-50 bg-warning-500"> Button </ButtonBasics>
+    </button-block>
+    <button-block class="text-neutral-50 bg-error-500"> Button </button-block>
+    <button-block class="text-neutral-50 bg-warning-500"> Button </button-block>
   </div>
   <div class="flex flex-col gap-4 mb-4">
-    <InputText v-model="inputText" placeholder="placeholder" />
+    <input-text v-model="inputText" placeholder="placeholder" />
   </div>
   <div class="flex flex-col gap-4">
-    <InputMultilineText
+    <input-multiline-text
       v-model="InputMultilineText"
       placeholder="placeholder"
     />
@@ -32,14 +32,14 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import ButtonBasics from '@/components/button/ButtonBasics.vue'
+import ButtonBlock from '@/components/button/ButtonBlock.vue'
 import InputMultilineText from '@/components/input/InputMultilineText.vue'
 import InputText from '@/components/input/InputText.vue'
 
 export default defineComponent({
   name: 'ViewHome',
   components: {
-    ButtonBasics,
+    ButtonBlock,
     InputMultilineText,
     InputText,
   },
