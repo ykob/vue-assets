e3a1e08c98483b7a81786d07326b7b94f258be9b
ykob <info@tplh.net>
2022-06-09 20:45:52 +0900

    added CheckboxBasics component.

---
+++ src/components/checkbox/CheckboxBasics.vue
@@ -0,0 +1,42 @@
+<template>
+  <label class="checkbox" :disabled="disabled">
+    <input
+      type="checkbox"
+      v-bind="$attrs"
+      :value="value"
+      :checked="checked"
+      :disabled="disabled"
+      @change="$emit('change', $event)"
+    />
+    <div></div>
+    <div>
+      <slot></slot>
+    </div>
+  </label>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'CheckboxBasics',
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+    checked: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    desc: {
+      type: Boolean,
+      default: false,
+    },
+  },
+})
+</script>
