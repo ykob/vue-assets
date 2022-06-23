deb0b1374a642e278c791aed8ee905f9736e4bd0
ykob <info@tplh.net>
2022-06-23 10:46:32 +0900

    changed the styles of IconSVG to tailwind classes.

--- src/components/icon/IconSvg.vue
+++ src/components/icon/IconSvg.vue
@@ -1,6 +1,7 @@
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
+    class="block fill-current"
     :height="px"
     viewBox="0 0 24 24"
     :width="px"
@@ -43,10 +44,3 @@ export default defineComponent({
   },
 })
 </script>
-
-<style lang="scss" scoped>
-svg {
-  display: block;
-  fill: currentColor;
-}
-</style>
