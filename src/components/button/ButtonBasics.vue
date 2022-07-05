010160faeea2023ddb1bdc139d20045f2d655899
ykob <info@tplh.net>
2022-07-05 19:36:55 +0900

    added loading prop on ButtonBasics.

--- src/components/button/ButtonBasics.vue
+++ src/components/button/ButtonBasics.vue
@@ -1,8 +1,9 @@
 <template>
   <component
     class="flex items-center justify-center"
+    :class="classnames"
     :is="tag"
-    @click="$emit('accepted')"
+    @click="handleClick"
   >
     <slot></slot>
   </component>
@@ -15,9 +16,28 @@ export default defineComponent({
   name: 'ButtonBasics',
   emits: ['accepted'],
   props: {
+    loading: {
+      type: Boolean,
+      default: false,
+    },
     tag: {
       type: String,
       default: 'button',
+    },
+  },
+  computed: {
+    classnames(): string[] {
+      if (this.loading === true) {
+        return ['pointer-events-none']
+      } else {
+        return []
+      }
+    },
+  },
+  methods: {
+    handleClick() {
+      if (this.loading === true) return
+      this.$emit('accepted')
     },
   },
 })
