8b42042e2ead7d897392bc6b599ab5b6dbc2beb0
ykob <info@tplh.net>
2022-07-19 16:02:21 +0900

    added InputImage component.

---
+++ src/components/form-element/InputImage.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="flex flex-col gap-3 items-baseline" :key="keyPrefix">
+    <input
+      class="hidden"
+      accept="image/png, image/jpeg"
+      :disabled="disabled"
+      :multiple="multiple"
+      ref="input"
+      type="file"
+      @change="changeHandler"
+    />
+    <div class="flex gap-2">
+      <button-block
+        button-type="information"
+        :icon="icons.image"
+        shrink
+        @click="onClickSelect"
+      >
+        Select Images
+      </button-block>
+      <button-block
+        v-if="selectedFiles"
+        button-type="destructive"
+        :icon="icons.close"
+        shrink
+        @click="onClickClear"
+      >
+        Clear
+      </button-block>
+    </div>
+    <div
+      v-if="!selectedFiles"
+      class="bg-neutral-100 border-2 border-dashed border-neutral-400 px-4 py-2 rounded"
+    >
+      No Images are selected.
+    </div>
+    <div
+      v-else
+      class="bg-information-50 border-2 border-dashed border-information-300 px-4 py-2 rounded"
+    >
+      <div v-for="(file, index) in modelValue" :key="keyPrefix + '-' + index">
+        {{ file.name }} ({{ file.size / 1000 }}kB)
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { mdiCloseCircle, mdiImage } from '@mdi/js'
+import ButtonBlock from '../button/ButtonBlock.vue'
+
+export default defineComponent({
+  components: { ButtonBlock },
+  name: 'InputFile',
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    error: {
+      type: Boolean,
+      default: false,
+    },
+    keyPrefix: {
+      type: String,
+      required: true,
+    },
+    modelValue: {
+      type: Object as PropType<FileList | null>,
+      default: null,
+    },
+    multiple: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    selectedFiles(): boolean {
+      return this.modelValue !== null && this.modelValue.length > 0
+    },
+  },
+  data: (): {
+    icons: { [key: string]: string }
+  } => ({
+    icons: {
+      image: mdiImage,
+      close: mdiCloseCircle,
+    },
+  }),
+  methods: {
+    changeHandler(event: Event): void {
+      const { target } = event
+      if (!(target instanceof HTMLInputElement)) return
+      this.$emit('update:modelValue', target.files)
+    },
+    onClickSelect() {
+      const { input } = this.$refs
+      if (!(input instanceof HTMLInputElement)) return
+      input.click()
+    },
+    onClickClear() {
+      this.$emit('update:modelValue', null)
+    },
+  },
+})
+</script>
